d9276181410fef261bf1b81b7a04ade9d4fdf9d7
HarpriyaGuru <guruharpriya@gmail.com>
2025-03-31 22:57:46 +0530

    add README file

--- src/Pages/Channel/Channel.jsx
+++ src/Pages/Channel/Channel.jsx
@@ -23,11 +23,11 @@ const Profile = ({ sidenav }) => {
                     {/* Profile Details */}
                     <div className="profile-content">
                         <div className="container-box">
-                            <div className="about-name-profile">Two Side Avengers</div>
-                            <div className="about-info">@Two Side • 1k subscribers • 18 videos</div>
-                            <div className="about-info">Hii guys, I am Antas raj pathak currently working as a frontend developer in Amazon</div>
+                            <div className="about-name-profile">My Channel</div>
+                            <div className="about-info">15k subscribers • 2 videos</div>
+                            <div className="about-info">Hii guys, I am priya currently working as a Software engineer  in Google</div>
                             <div className="about-info">
-                                <a href="#" className="profile-link">port-b7439.web.app</a> and <span className="profile-link">2 more links</span>
+                                <a href="#" className="profile-link">port-b74454.web.app</a> and <span className="profile-link">3 more links</span>
                             </div>
 
                             {/* Buttons */}
@@ -58,7 +58,7 @@ const Profile = ({ sidenav }) => {
                                     <img className='img-yt-popular' src="https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010130.jpg?semt=ais_hybrid" alt="pic1" />
                                 </div>
                                 <div className="about-yt">
-                                    <div className="title-popular">How to derriev ui ux fromula from a ingine</div>
+                                    <div className="title-popular">How to create react files</div>
                                     <div className="info-popular">2k views  .  8 month ago</div>
                                 </div>
                             </Link>
